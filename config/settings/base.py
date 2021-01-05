2ee7e5a17895e1e1c27339f99b718e3517a71439
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2021-01-05 10:04:49 +0200

    Allow localhost:80 through CORS

--- config/settings/base.py
+++ config/settings/base.py
@@ -280,6 +280,7 @@ REST_FRAMEWORK = {
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r".*/api/.*$"
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
     "http://localhost:3000"
 ]
 # Your stuff...
